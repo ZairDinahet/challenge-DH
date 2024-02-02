20ee940baa50c271801ffef6dbe820beb9f350a9
rociocala <rpciocala@gmail.com>
2024-02-01 23:52:48 -0300

    feat: sequelize installation

---
+++ server/src/database/models/Applicant.js
@@ -0,0 +1,67 @@
+module.exports = (sequelize, DataTypes) => {
+    const Applicant = sequelize.define('Applicant', {
+        id: {
+            type: DataTypes.BIGINT(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            unique: true
+        },
+        numberPhone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true        
+        },
+        linkedIn: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            unique: true
+        },
+        birthDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING(50),
+        },
+        aboutMe: {
+            type: DataTypes.TEXT,
+        },
+        city: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        country: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+    },
+    {
+        timestamps: false,
+        tableName: 'applicants', 
+    })
+
+    Applicant.associate = (models) => {
+        Applicant.belongsToMany(models.Profession, {
+          as: 'professions',
+          through: 'applicantsprofessions',
+          foreignKey: 'idApplicant',
+          otherKey: 'idProfession',
+          timestamps: false
+        })
+    }
+    
+    return Applicant;
+}
