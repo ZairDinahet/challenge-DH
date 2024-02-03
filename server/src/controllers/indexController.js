34113accce0499b946f332bde69fb81f80e68350
rociocala <rpciocala@gmail.com>
2024-02-02 22:31:34 -0300

    feat: sequelize installation

--- server/src/controllers/indexController.js
+++ server/src/controllers/indexController.js
@@ -1,6 +1,12 @@
+const db = require('../database/models')
 const indexController = {
   index: function(req, res){
     res.send("Hola, server levantado")
+  },
+  // por el momento para que se creen las tablas
+  findAll: async function(req, res){
+    const applicants = db.Applicant.findAll()
+    const professions = db.Profession.findAll()
   }
 }
 
